97c9a1724589285fe695860cdbc2c6fef126f3e9
Bela Hamid <belahamid@gmail.com>
2024-12-06 14:18:59 +0000

    add logging, and create launches model

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
 
@@ -10,7 +12,14 @@ app.use(
     options: "http://localhost:3000",
   })
 );
+app.use(morgan("combined"));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
