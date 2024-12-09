9b794b7daa66edfe21f485a4d8d6902a88230df5
Bela Hamid <belahamid@gmail.com>
2024-12-09 15:31:00 +0000

    connect FE to BE for adding launches

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -6,7 +6,20 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
   const launch = req.body;
-  launch.launchDate = new Date(launch.launchDate);
+  const { mission, rocket, target } = launch;
+  let { launchDate } = launch;
+
+  if (!mission || !launchDate || !rocket || !target) {
+    return res.status(400).json({
+      error: "missing required launch property",
+    });
+  }
+  launchDate = new Date(launchDate);
+  if (isNaN(launchDate)) {
+    return res.status(400).json({
+      error: "Invalid launch date",
+    });
+  }
   addNewLaunch(launch);
   return res.status(201).json(launch);
 }
