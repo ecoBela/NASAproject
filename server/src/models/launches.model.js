2acad0419307736bfea39c6101769df4ef8ec7fc
Bela Hamid <belahamid@gmail.com>
2024-12-09 13:33:48 +0000

    Create POST endpoint for adding new launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -6,15 +6,35 @@ const launch = {
   rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
   destination: "Kepler-442 b",
-  customer: ["ZTM", "NASA"],
+  customers: ["ZTM", "NASA"],
   upcoming: true,
   success: true,
 };
 
+let latestFlightNumber = 100;
+
 launches.set(launches.flightNumber, launch);
+
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+
+  return launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      mission: launch.mission,
+      rocket: launch.rocket,
+      launchDate: new Date(launch.launchDate),
+      destination: launch.destination,
+      customers: ["ZTM", "NASA"],
+      upcoming: true,
+      success: true,
+      flightNumber: latestFlightNumber,
+    })
+  );
+}
 
 function getAllLaunches() {
   return Array.from(launches.values());
 }
 
-module.exports = { getAllLaunches };
+module.exports = { getAllLaunches, addNewLaunch };
