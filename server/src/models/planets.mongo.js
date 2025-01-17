07f5130b00c094fb71fd61c386931ea0b8daa014
Bela Hamid <belahamid@gmail.com>
2025-01-17 10:55:56 +0000

    Create schemas for planets and launch data

---
+++ server/src/models/planets.mongo.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const planetsSchema = new mongoose.Schema({
+  keplerName: { type: String, required: true },
+});
+
+const planets = mongoose.model("Planet", planetsSchema);
+module.exports = planets;
