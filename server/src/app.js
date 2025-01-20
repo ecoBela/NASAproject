2a65ccdd67210cc65897e540c4063cbf51e1f9d2
Bela Hamid <belahamid@gmail.com>
2025-01-20 14:25:38 +0000

    create api versioning

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,9 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const api = require("./routes/api");
 
 const app = express();
-
-const planetsRouter = require("./routes/planets/planets.router");
-const launchesRouter = require("./routes/launches/launches.router");
 
 app.use(
   cors({
@@ -19,9 +17,8 @@ app.use(morgan("combined"));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
+app.use("/v1", api);
 
-app.use("/planets", planetsRouter);
-app.use("/launches", launchesRouter);
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
