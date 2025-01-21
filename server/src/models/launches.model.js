ce26de6b03b7a12da0034714113d6b29ca274cda
Bela Hamid <belahamid@gmail.com>
2025-01-21 19:23:51 +0000

    Load data from SpaceX api and map to database

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,20 +1,56 @@
+const axios = require("axios");
 const launchesDatabase = require("./launches.mongo");
 const planets = require("./planets.mongo");
 
 const launch = {
-  flightNumber: 100,
-  mission: "Kepler Exploration X",
-  rocket: "Explorer IS1",
-  launchDate: new Date("December 27, 2030"),
-  target: "Kepler-442 b",
-  customers: ["ZTM", "NASA"],
-  upcoming: true,
-  success: true,
+  flightNumber: 100, // flight_number
+  mission: "Kepler Exploration X", // name
+  rocket: "Explorer IS1", // rocket.name
+  launchDate: new Date("December 27, 2030"), //date_local
+  target: "Kepler-442 b", // not applicable
+  customers: ["ZTM", "NASA"], // payload.customers for each payload
+  upcoming: true, //upcoming
+  success: true, //success
 };
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
 saveLaunch(launch);
+
+async function loadLaunchesData() {
+  const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query";
+  const response = await axios.post(
+    "https://api.spacexdata.com/v4/launches/query",
+    {
+      query: {},
+      options: {
+        populate: [
+          { path: "rocket", select: { name: 1 } },
+          { path: "payloads", select: { customers: 1 } },
+        ],
+      },
+    }
+  );
+  console.log("Downloading launch data...");
+  const launchDocs = response.data.docs;
+  for (const launchDoc of launchDocs) {
+    const payloads = launchDoc["payloads"];
+    const customers = payloads.flatMap((payload) => {
+      return payload["customers"];
+    });
+
+    const launch = {
+      flightNumber: launchDoc["flight_number"],
+      mission: launchDoc["name"],
+      rocket: launchDoc["rocket"]["name"],
+      launchDate: launchDoc["date_local"],
+      upcoming: launchDoc["upcoming"],
+      success: launchDoc["success"],
+      customers,
+    };
+    console.log(`${launch.flightNumber} ${launch.mission}`);
+  }
+}
 
 async function getLastestFlightNumber() {
   const latestLaunch = await launchesDatabase.findOne().sort("-flightNumber");
@@ -69,6 +105,7 @@ async function saveLaunch(launch) {
 }
 
 module.exports = {
+  loadLaunchesData,
   scheduleNewLaunch,
   existsLaunchWithId,
   abortWithLaunchId,
